6868f3371bc10e1984d6d8b7241a8f91184bd572
Xianzhe Liang <liangxianzhe@gmail.com>
2023-05-26 11:39:21 -0700

    Skip if the input doesn't change.

--- lua/floating-input.lua
+++ lua/floating-input.lua
@@ -56,7 +56,9 @@ function M.input(opts, on_confirm, win_config)
   -- Enter to confirm
   vim.keymap.set({ 'n', 'i', 'v' }, '<cr>', function()
     local lines = vim.api.nvim_buf_get_lines(buffer, 0, 1, false)
-    if on_confirm then on_confirm(lines[1]) end
+    if lines[1] ~= default and on_confirm then
+      on_confirm(lines[1])
+    end
     vim.api.nvim_win_close(window, true)
     vim.cmd('stopinsert')
   end, { buffer = buffer })
